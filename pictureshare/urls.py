d6e80972bee92c608578191c96cef9afe9998112
Martin Nyandigisi <matonyando@gmail.com>
2026-06-17 16:56:22 +0300

    Fix media files not serving in production by adding serve view to urls.py

--- pictureshare/urls.py
+++ pictureshare/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('pictures.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
